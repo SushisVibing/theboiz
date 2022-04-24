bff86d3cb9fc190f796e369c0496445134692568
darksun123e <pushe@DESKTOP-6OJR1CH>
2022-04-23 21:24:13 -0500

    milking update

--- src/main/java/net/mcreator/theboyssa/entity/PenisEntity.java
+++ src/main/java/net/mcreator/theboyssa/entity/PenisEntity.java
@@ -14,6 +14,7 @@ import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
@@ -27,12 +28,15 @@ import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.protocol.Packet;
 
-import net.mcreator.theboyssa.init.TheboyssaModItems;
+import net.mcreator.theboyssa.procedures.MilkingProcedure;
 import net.mcreator.theboyssa.init.TheboyssaModEntities;
 
 @Mod.EventBusSubscriber
@@ -72,11 +76,6 @@ public class PenisEntity extends PathfinderMob {
 		return MobType.UNDEFINED;
 	}
 
-	protected void dropCustomDeathLoot(DamageSource source, int looting, boolean recentlyHitIn) {
-		super.dropCustomDeathLoot(source, looting, recentlyHitIn);
-		this.spawnAtLocation(new ItemStack(TheboyssaModItems.CUMDROP));
-	}
-
 	@Override
 	public SoundEvent getHurtSound(DamageSource ds) {
 		return ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.generic.hurt"));
@@ -85,6 +84,21 @@ public class PenisEntity extends PathfinderMob {
 	@Override
 	public SoundEvent getDeathSound() {
 		return ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.generic.death"));
+	}
+
+	@Override
+	public InteractionResult mobInteract(Player sourceentity, InteractionHand hand) {
+		ItemStack itemstack = sourceentity.getItemInHand(hand);
+		InteractionResult retval = InteractionResult.sidedSuccess(this.level.isClientSide());
+		super.mobInteract(sourceentity, hand);
+		double x = this.getX();
+		double y = this.getY();
+		double z = this.getZ();
+		Entity entity = this;
+		Level world = this.level;
+
+		MilkingProcedure.execute(entity, sourceentity, itemstack);
+		return retval;
 	}
 
 	public static void init() {
---
+++ src/main/java/net/mcreator/theboyssa/procedures/MilkingProcedure.java
@@ -0,0 +1,31 @@
+package net.mcreator.theboyssa.procedures;
+
+import net.minecraftforge.items.ItemHandlerHelper;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+
+import net.mcreator.theboyssa.init.TheboyssaModItems;
+
+public class MilkingProcedure {
+	public static void execute(Entity entity, Entity sourceentity, ItemStack itemstack) {
+		if (entity == null || sourceentity == null)
+			return;
+		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == TheboyssaModItems.SOCK) {
+			if (sourceentity instanceof Player _player) {
+				ItemStack _stktoremove = new ItemStack(TheboyssaModItems.SOCK);
+				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1,
+						_player.inventoryMenu.getCraftSlots());
+			}
+			if (sourceentity instanceof Player _player) {
+				ItemStack _setstack = new ItemStack(TheboyssaModItems.CUMSOCK);
+				_setstack.setCount(1);
+				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+			}
+			if (entity instanceof Player _player)
+				_player.getCooldowns().addCooldown(itemstack.getItem(), 6600);
+		}
+	}
+}
