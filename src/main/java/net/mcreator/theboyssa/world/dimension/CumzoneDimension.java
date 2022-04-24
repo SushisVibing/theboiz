a01f770d69959a0483482843aad373db637bd3c4
sushisvibing <golde@192.168.1.198>
2022-04-23 19:51:00 -0500

    super cumsock made

--- src/main/java/net/mcreator/theboyssa/world/dimension/CumzoneDimension.java
+++ src/main/java/net/mcreator/theboyssa/world/dimension/CumzoneDimension.java
@@ -6,21 +6,15 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.levelgen.carver.WorldCarver;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.core.Registry;
 import net.minecraft.client.renderer.DimensionSpecialEffects;
 
-import net.mcreator.theboyssa.procedures.CumzonePlayerEntersDimensionProcedure;
 import net.mcreator.theboyssa.init.TheboyssaModBlocks;
 
 import java.util.Set;
@@ -63,19 +57,6 @@ public class CumzoneDimension {
 				}
 			};
 			event.enqueueWork(() -> DimensionSpecialEffects.EFFECTS.put(new ResourceLocation("theboyssa:cumzone"), customEffect));
-		}
-	}
-
-	@SubscribeEvent
-	public void onPlayerChangedDimensionEvent(PlayerEvent.PlayerChangedDimensionEvent event) {
-		Entity entity = event.getPlayer();
-		Level world = entity.level;
-		double x = entity.getX();
-		double y = entity.getY();
-		double z = entity.getZ();
-		if (event.getTo() == ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation("theboyssa:cumzone"))) {
-
-			CumzonePlayerEntersDimensionProcedure.execute();
 		}
 	}
 }
